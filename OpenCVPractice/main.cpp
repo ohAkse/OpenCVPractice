10e3af4606c38dfba00663760b7a65773621808e
ohAkse <segassdc1@naver.com>
2023-12-04 13:55:14 +0900

    gitignore 및  세팅설정

--- OpenCVPractice/main.cpp
+++ OpenCVPractice/main.cpp
@@ -7,23 +7,52 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+
+#define Chapter_1222
+
 using namespace std;
 using namespace cv;
+
+std::string imagePath = "/Users/segassdc/Downloads/a.png";
+
 int main(int argc, const char * argv[]) {
-    std::string imagePath = "/Users/segassdc/Downloads/a.png";
+    
+#ifdef CV_SETTING
+    Mat image = cv::imread(imagePath);
 
-    // 이미지 읽기
-    cv::Mat image = cv::imread(imagePath);
-
-    // 이미지 읽기에 성공한 경우
     if (!image.empty()) {
-        cv::imshow("Loaded Image", image);
-        cv::waitKey(0);
+        imshow("이미지 갱신", image);
+        waitKey(0);
     } else {
-        std::cout << "Failed to load the image!" << std::endl;
+        cout << "이미지 갱신 실패" << endl;
     }
+#elifdef Chapter_1
+    cout<<"Chapter1"<<endl;
+#elifdef Chapter_2
+    cout<<"Chapter2"<<endl;
+#elifdef Chapter_3
+    cout<<"Chapter3"<<endl;
+#elifdef Chapter_4
+    cout<<"Chapter4"<<endl;
+#elifdef Chapter_5
+    cout<<"Chapter5"<<endl;
+#elifdef Chapter_6
+    cout<<"Chapter6"<<endl;
+#elifdef Chapter_7
+    cout<<"Chapter7"<<endl;
+#elifdef Chapter_8
+    cout<<"Chapter8"<<endl;
+#elifdef Chapter_9
+    cout<<"Chapter9"<<endl;
+#elifdef Chapter_10
+    cout<<"Chapter10"<<endl;
+#elifdef Chapter_11
+    cout<<"Chapter11"<<endl;
+#else
+    cout<<"Macro is in the wrong format or not set"<<endl;
+#endif
 
-    waitKey(0); // 키 입력을 대기합니다.
+    waitKey(0);
 
     return 0;
 }
